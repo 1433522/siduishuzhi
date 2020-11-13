82a674de94fccc2ea0c048d629e7fb7c6dc4ffc8
zzx <me@zhangzhixiong.com>
2020-11-13 09:35:18 +0800

    前端界面进行了美化，述职流程进一步优化。

--- shuzhi/views.py
+++ shuzhi/views.py
@@ -5,10 +5,12 @@ from .models import Addr,Shuzhizhe
 def index(request):
     # 未述职者随机进行述职: 如果你想进行随机排序,可以直接传入'?'即可。
     status0 = Shuzhizhe.objects.filter(status=0).order_by('?',)[:8]
-    print(status0)
     # 下次述职和述职完毕的人按照营区排序
     status1 = Shuzhizhe.objects.filter(status=1).order_by('?')[:8]
     status2 = Shuzhizhe.objects.filter(status=2).order_by('?')[:8]
+    print("status0:"+str(status0))
+    print("status1:"+str(status1))
+    print("status2:"+str(status2))
 
     return render(request,
             'shuzhi/index.html',
